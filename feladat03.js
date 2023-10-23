40275c79100aa23b33909c057cd9facf33f4e3a9
Abradave <daciabra@gmail.com>
2023-10-23 09:04:09 +0200

    feladat1 javitas

---
+++ feladat03.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", init);
+
+function init() {
+    var lista = [0, 7, 16, 30, 41];
+    for (let i = 0; i < lista.length; i++) {
+        console.log(lista[i]);
+    }
+}
